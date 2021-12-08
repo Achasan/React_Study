94446a5200ad2dd006e2cff3b32b68602467f1bd
Achasan <79975278+Achasan@users.noreply.github.com>
2021-12-08 13:49:31 +0900

    useState 개념 익히기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  let [title, modifyFuction] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "스타트업 들어가는 법",
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="black-nav">
+        <div>AchaSan</div>
+      </div>
+      <div className="list">
+        <h4>{title[0]}</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {month}월 {day}일 발행
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>
+          {month}월 {day}일 발행
+        </p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>
+          {month}월 {day}일 발행
+        </p>
+        <hr />
+      </div>
     </div>
   );
 }
